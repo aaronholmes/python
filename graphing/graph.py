6f1340b8caf2d4cff5e847fc53a47ba7a1864f29
aaronholmes <aholmes@pureguru-software.com>
2019-03-16 18:30:17 -0400

    Added more logging for undirected graph

--- graphing/graph.py
+++ graphing/graph.py
@@ -82,10 +82,21 @@ class AdjacencyMatrixGraph(Graph):
                 print(i, "-->", v)
 
 
-graph = AdjacencyMatrixGraph(4)
+vertices = 4
+graph = AdjacencyMatrixGraph(vertices)
 
 graph.add_edge(0, 1)
 graph.add_edge(0, 2)
 graph.add_edge(2, 3)
+
+for i in range(vertices):
+    print("Adjacent to: ", i, graph.get_adjacent_vertices(i))
+
+for i in range(vertices):
+    print("Indegree: ", i, graph.get_indegree(i))
+
+for i in range(vertices):
+    for j in graph.get_adjacent_vertices(i):
+        print("Edge weight: ", i, " ", j, " weight: ", graph.get_edge_weight(i, j))
 
 graph.display()
