7d606cbdb17a7b007248f9e4e571fa7581b8dba4
aaronholmes <aholmes@pureguru-software.com>
2019-03-16 18:33:46 -0400

    cleaned up output and added directed graph example

--- graphing/graph.py
+++ graphing/graph.py
@@ -82,21 +82,34 @@ class AdjacencyMatrixGraph(Graph):
                 print(i, "-->", v)
 
 
+def graph_print_log(graph, vertices):
+    for i in range(vertices):
+        print("Adjacent to: ", i, graph.get_adjacent_vertices(i))
+
+    for i in range(vertices):
+        print("Indegree: ", i, graph.get_indegree(i))
+
+    for i in range(vertices):
+        for j in graph.get_adjacent_vertices(i):
+            print("Edge weight: ", i, " ", j, " weight: ", graph.get_edge_weight(i, j))
+
+    graph.display()
+
+
 vertices = 4
+
 graph = AdjacencyMatrixGraph(vertices)
 
 graph.add_edge(0, 1)
 graph.add_edge(0, 2)
 graph.add_edge(2, 3)
 
-for i in range(vertices):
-    print("Adjacent to: ", i, graph.get_adjacent_vertices(i))
+graph_print_log(graph, vertices)
 
-for i in range(vertices):
-    print("Indegree: ", i, graph.get_indegree(i))
+graph2 = AdjacencyMatrixGraph(vertices, True)
 
-for i in range(vertices):
-    for j in graph.get_adjacent_vertices(i):
-        print("Edge weight: ", i, " ", j, " weight: ", graph.get_edge_weight(i, j))
+graph2.add_edge(0, 1)
+graph2.add_edge(0, 2)
+graph2.add_edge(2, 3)
 
-graph.display()
+graph_print_log(graph2, vertices)
